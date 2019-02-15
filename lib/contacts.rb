0eca53c9b68dcfba1f56a1d82cf2339087c70c8d
Alicia Santiago <albrown07@mac.com>
2019-02-15 17:45:00 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -19,5 +19,8 @@ def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
     contact_details_hash.each do |attribute, data|
     binding.pry
+    
+    
+   end
   end
 end
